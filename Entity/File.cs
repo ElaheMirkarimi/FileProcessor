7e4167749d8aefb578388c60973d67070fca56f3
Elahe Mirkarimi <mirkarimi.elahe@outlook.com>
2019-07-24 14:13:06 -0400

    Entity and Infra.Data are changed.

---
+++ Entity/File.cs
@@ -0,0 +1,12 @@
+﻿namespace Entity
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public short Month { get; set; }
+        public string Guid { get; set; }
+        public short Version { get; set; }
+    }
+}
